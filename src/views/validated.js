2ed1c677ec5896f552c2245f09f03b79ff5c0aec
Ross-Clark <ross.clark2@nhs.net>
2019-07-18 13:41:04 +0100

    validating style

--- src/views/validated.js
+++ src/views/validated.js
@@ -4,14 +4,14 @@ class Validated extends React.Component {
 
     render(){
         return ( 
-            <div>
+            <div className='validated'>
                 <div className='validated-container jumbotron'>
                     <div className='validated-contents text-center'>
-                        <h1 className='text-success'>Approved!</h1>
-                        <br/>
+                        <h1 className='text-success display-4'>Approved!</h1>
+                        <hr className='hr-4'></hr>
                         <p className='text-body'> The prescription can now be passed on to the Pharmacist </p>
                         <br/>
-                        <a className='btn btn-primary text-white' href='/index'>New prescription</a>
+                        <a className='btn btn-primary text-white' href='/'>New prescription</a>
                     </div>
                 </div>
             </div>
