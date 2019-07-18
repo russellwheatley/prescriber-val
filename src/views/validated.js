d49b840cabd36e4873a7509069f20055fb314497
Ross-Clark <ross.clark2@nhs.net>
2019-07-18 09:18:29 +0100

    init validated page

--- src/views/validated.js
+++ src/views/validated.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-function Validated() {
-    return <div className="validated">validated</div>;
-}
+class Validated extends React.Component {
 
+    render(){
+        return ( 
+            <div>
+                <div className='validated-container jumbotron'>
+                    <div className='validated-contents text-center'>
+                        <h1 className='text-success'>Approved!</h1>
+                        <br/>
+                        <p className='text-body'> The prescription can now be passed on to the Pharmacist </p>
+                        <br/>
+                        <a className='btn btn-primary text-white' href='/index'>New prescription</a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
 export default Validated;
